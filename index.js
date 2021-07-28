861be2de8b4e01d60c8e66c4ee18bac9ae38f586
Chris Germishuys <chris.germishuys@gmail.com>
2021-07-28 10:11:59 -0400

    Adds express urlencoded line

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import cors from 'cors'
 const port = process.env.PORT || 4000
 const app = express()
 app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/', routes)
